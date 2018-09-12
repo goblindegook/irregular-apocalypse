8d6ed6028914acc0993f3242e6415243e2c297c9
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-09-12 16:25:39 +0100

    Fix issue with month name for January

--- src/calendar.spec.ts
+++ src/calendar.spec.ts
@@ -1,6 +1,6 @@
 import { property, integer } from 'jsverify'
 import { all } from 'ramda'
-import { currentMonth, defaultMonthData } from './calendar'
+import { currentMonth, defaultMonthData, monthName } from './calendar'
 
 describe('defaultMonth()', () => {
   it('returns the month number and year', () => {
@@ -27,13 +27,21 @@ describe('defaultMonthData()', () => {
     expect(Object.keys(defaultMonthData(2020, 2))).toHaveLength(29)
   })
 
-  property('generates first day with key 1', integer(2000, 2100), integer(1, 12),
+  property(
+    'generates first day with key 1',
+    integer(2000, 2100),
+    integer(1, 12),
     (year, month) => Object.keys(defaultMonthData(year, month))[0] === '1'
   )
 
-  property('generates incremental keys', integer(2000, 2100), integer(1, 12),
+  property(
+    'generates incremental keys',
+    integer(2000, 2100),
+    integer(1, 12),
     (year, month) => {
-      const days = Object.keys(defaultMonthData(year, month)).map(v => parseInt(v, 10))
+      const days = Object.keys(defaultMonthData(year, month)).map(v =>
+        parseInt(v, 10)
+      )
       return all(([v, i]) => v === i + 1, days.map((v, i) => [v, i]))
     }
   )
@@ -42,31 +50,61 @@ describe('defaultMonthData()', () => {
     return [date.getHours(), date.getMinutes()]
   }
 
-  property('starts the AM period at 9:00', integer(2000, 2100), integer(1, 12),
-    (year, month) => all(
-      ([h, m]) => h === 9 && m === 0,
-      Object.values(defaultMonthData(year, month)).map(({ am }) => getTime(am.starts))
-    )
+  property(
+    'starts the AM period at 9:00',
+    integer(2000, 2100),
+    integer(1, 12),
+    (year, month) =>
+      all(
+        ([h, m]) => h === 9 && m === 0,
+        Object.values(defaultMonthData(year, month)).map(({ am }) =>
+          getTime(am.starts)
+        )
+      )
   )
 
-  property('ends the AM period at 13:00', integer(2000, 2100), integer(1, 12),
-    (year, month) => all(
-      ([h, m]) => h === 13 && m === 0,
-      Object.values(defaultMonthData(year, month)).map(({ am }) => getTime(am.ends))
-    )
+  property(
+    'ends the AM period at 13:00',
+    integer(2000, 2100),
+    integer(1, 12),
+    (year, month) =>
+      all(
+        ([h, m]) => h === 13 && m === 0,
+        Object.values(defaultMonthData(year, month)).map(({ am }) =>
+          getTime(am.ends)
+        )
+      )
   )
 
-  property('starts the PM period at 14:00', integer(2000, 2100), integer(1, 12),
-    (year, month) => all(
-      ([h, m]) => h === 14 && m === 0,
-      Object.values(defaultMonthData(year, month)).map(({ pm }) => getTime(pm.starts))
-    )
+  property(
+    'starts the PM period at 14:00',
+    integer(2000, 2100),
+    integer(1, 12),
+    (year, month) =>
+      all(
+        ([h, m]) => h === 14 && m === 0,
+        Object.values(defaultMonthData(year, month)).map(({ pm }) =>
+          getTime(pm.starts)
+        )
+      )
   )
 
-  property('ends the PM period at 17:30', integer(2000, 2100), integer(1, 12),
-    (year, month) => all(
-      ([h, m]) => h === 17 && m === 30,
-      Object.values(defaultMonthData(year, month)).map(({ pm }) => getTime(pm.ends))
-    )
+  property(
+    'ends the PM period at 17:30',
+    integer(2000, 2100),
+    integer(1, 12),
+    (year, month) =>
+      all(
+        ([h, m]) => h === 17 && m === 30,
+        Object.values(defaultMonthData(year, month)).map(({ pm }) =>
+          getTime(pm.ends)
+        )
+      )
   )
+})
+
+describe('monthName()', () => {
+  it('returns January 2019', () => {
+    expect(monthName(2018, 1)).toBe('January 2018')
+  })
 })
--- src/calendar.ts
+++ src/calendar.ts
@@ -23,34 +23,36 @@ export type Periods = {
 function monthData (year: number, month: number): Day[] {
   const daysInMonth = getDaysInMonth(new Date(year, month - 1))
 
-  return range(1, daysInMonth + 1)
-    .map(day => {
-      const date = new Date(year, month - 1, day)
-      const checked = !isWeekend(date)
-      return {
-        am: {
-          starts: new Date(date.setHours(9, 0)),
-          ends: new Date(date.setHours(13, 0)),
-          checked
-        },
-        pm: {
-          starts: new Date(date.setHours(14, 0)),
-          ends: new Date(date.setHours(17, 30)),
-          checked
-        }
+  return range(1, daysInMonth + 1).map(day => {
+    const date = new Date(year, month - 1, day)
+    const checked = !isWeekend(date)
+    return {
+      am: {
+        starts: new Date(date.setHours(9, 0)),
+        ends: new Date(date.setHours(13, 0)),
+        checked
+      },
+      pm: {
+        starts: new Date(date.setHours(14, 0)),
+        ends: new Date(date.setHours(17, 30)),
+        checked
       }
-    })
+    }
+  })
 }
 
 function addKeys (month: Day[]): Month {
-  return month.reduce<Month>((acc, day, idx) => ({ ...acc, [`${idx + 1}`]: day }), {})
+  return month.reduce<Month>(
+    (acc, day, idx) => ({ ...acc, [`${idx + 1}`]: day }),
+    {}
+  )
 }
 
 export function defaultMonthData (year: number, month: number): Month {
   return addKeys(monthData(year, month))
 }
 
-export function currentMonth (): { month: number, year: number } {
+export function currentMonth (): { month: number; year: number } {
   const date = new Date()
   return {
     month: date.getMonth() + 1,
@@ -61,7 +63,7 @@ export function currentMonth (): { month: number, year: number } {
 export function monthName (year: number, month: number): string {
   const currentDate = new Date()
   const displayYear = year || currentDate.getFullYear()
-  const displayMonthIndex = month - 1 || currentDate.getMonth()
+  const displayMonthIndex = month ? month - 1 : currentDate.getMonth()
   const displayDate = new Date(displayYear, displayMonthIndex)
 
   return format(displayDate, 'MMMM YYYY')
