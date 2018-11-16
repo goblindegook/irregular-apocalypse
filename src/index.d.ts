769927e57c9d1ece39137b08a135fa614719cb39
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-11-16 18:25:53 +0000

    Removes unused type

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,9 +2,3 @@ declare module '*.css' {
   const CSS: any
   export default CSS
 }
-
-type RecursivePartial<T> = {
-  [P in keyof T]?: T[P] extends (infer U)[]
-    ? RecursivePartial<U>[]
-    : T[P] extends object ? RecursivePartial<T[P]> : T[P]
-}
