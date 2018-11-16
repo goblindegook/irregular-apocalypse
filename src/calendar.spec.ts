efc456e4ba368694643fbfb3c615ff1faccb82b6
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-11-16 16:52:51 +0000

    Updated dependencies

--- src/calendar.spec.ts
+++ src/calendar.spec.ts
@@ -1,4 +1,4 @@
-import { property, integer } from 'jsverify'
+import { assert, property, integer } from 'fast-check'
 import { all } from 'ramda'
 import { currentMonth, defaultMonthData, monthName } from './calendar'
 
@@ -27,49 +27,72 @@ describe('defaultMonthData()', () => {
     expect(Object.keys(defaultMonthData(2020, 2))).toHaveLength(29)
   })
 
-  property(
-    'generates first day with key 1',
-    integer(2000, 2100),
-    integer(1, 12),
-    (year, month) => Object.keys(defaultMonthData(year, month))[0] === '1'
-  )
+  it('generates first day with key 1', () => {
+    assert(
+      property(
+        integer(2000, 2100),
+        integer(1, 12),
+        (year, month) => Object.keys(defaultMonthData(year, month))[0] === '1'
+      )
+    )
+  })
 
-  property('generates incremental keys', integer(2000, 2100), integer(1, 12), (year, month) => {
-    const days = Object.keys(defaultMonthData(year, month)).map(v => parseInt(v, 10))
-    return all(([v, i]) => v === i + 1, days.map((v, i) => [v, i]))
+  it('generates incremental keys', () => {
+    assert(
+      property(integer(2000, 2100), integer(1, 12), (year, month) => {
+        const days = Object.keys(defaultMonthData(year, month)).map(v => parseInt(v, 10))
+        return all(([v, i]) => v === i + 1, days.map((v, i) => [v, i]))
+      })
+    )
   })
 
   function getTime(date: Date): [number, number] {
     return [date.getHours(), date.getMinutes()]
   }
 
-  property('starts the AM period at 9:00', integer(2000, 2100), integer(1, 12), (year, month) =>
-    all(
-      ([h, m]) => h === 9 && m === 0,
-      Object.values(defaultMonthData(year, month)).map(({ am }) => getTime(am.starts))
+  it('starts the AM period at 9:00', () => {
+    assert(
+      property(integer(2000, 2100), integer(1, 12), (year, month) =>
+        all(
+          ([h, m]) => h === 9 && m === 0,
+          Object.values(defaultMonthData(year, month)).map(({ am }) => getTime(am.starts))
+        )
+      )
     )
-  )
+  })
 
-  property('ends the AM period at 13:00', integer(2000, 2100), integer(1, 12), (year, month) =>
-    all(
-      ([h, m]) => h === 13 && m === 0,
-      Object.values(defaultMonthData(year, month)).map(({ am }) => getTime(am.ends))
+  it('ends the AM period at 13:00', () => {
+    assert(
+      property(integer(2000, 2100), integer(1, 12), (year, month) =>
+        all(
+          ([h, m]) => h === 13 && m === 0,
+          Object.values(defaultMonthData(year, month)).map(({ am }) => getTime(am.ends))
+        )
+      )
     )
-  )
+  })
 
-  property('starts the PM period at 14:00', integer(2000, 2100), integer(1, 12), (year, month) =>
-    all(
-      ([h, m]) => h === 14 && m === 0,
-      Object.values(defaultMonthData(year, month)).map(({ pm }) => getTime(pm.starts))
+  it('starts the PM period at 14:00', () => {
+    assert(
+      property(integer(2000, 2100), integer(1, 12), (year, month) =>
+        all(
+          ([h, m]) => h === 14 && m === 0,
+          Object.values(defaultMonthData(year, month)).map(({ pm }) => getTime(pm.starts))
+        )
+      )
     )
-  )
+  })
 
-  property('ends the PM period at 17:30', integer(2000, 2100), integer(1, 12), (year, month) =>
-    all(
-      ([h, m]) => h === 17 && m === 30,
-      Object.values(defaultMonthData(year, month)).map(({ pm }) => getTime(pm.ends))
+  it('ends the PM period at 17:30', () => {
+    assert(
+      property(integer(2000, 2100), integer(1, 12), (year, month) =>
+        all(
+          ([h, m]) => h === 17 && m === 30,
+          Object.values(defaultMonthData(year, month)).map(({ pm }) => getTime(pm.ends))
+        )
+      )
     )
-  )
+  })
 })
 
 describe('monthName()', () => {
