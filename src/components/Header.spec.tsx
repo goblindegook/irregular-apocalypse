d374e88c301bd6297dfa58d05bfddec499147fa8
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-04-16 17:21:09 +0100

    Custom render function

--- src/components/Header.spec.tsx
+++ src/components/Header.spec.tsx
@@ -2,39 +2,33 @@ import { h } from 'preact'
 import { cleanup, render, fireEvent } from 'preact-testing-library'
 import { Header } from './Header'
 
+function renderHeader(year: number, month: number) {
+  return render(
+    <Header month={month} year={year} onNameChange={jest.fn()} onSignatureChange={jest.fn()} />
+  )
+}
+
 describe('Header', () => {
   beforeEach(cleanup)
 
-  const noop = () => {
-    /* noop */
-  }
-
   it('renders month name and year in the title', () => {
-    const { getByText } = render(
-      <Header month={12} year={2012} onNameChange={noop} onSignatureChange={noop} />
-    )
+    const { getByText } = renderHeader(2012, 12)
     expect(getByText('December 2012')).toBeTruthy()
   })
 
   it('links to the previous month', () => {
-    const { getByTitle } = render(
-      <Header month={1} year={2018} onNameChange={noop} onSignatureChange={noop} />
-    )
+    const { getByTitle } = renderHeader(2018, 1)
     expect(getByTitle('Previous').getAttribute('href')).toBe('/2017/12')
   })
 
   it('links to the following month', () => {
-    const { getByTitle } = render(
-      <Header month={12} year={2017} onNameChange={noop} onSignatureChange={noop} />
-    )
+    const { getByTitle } = renderHeader(2017, 12)
     expect(getByTitle('Next').getAttribute('href')).toBe('/2018/01')
   })
 
   it('sets the default name', () => {
     const name = 'Test Name'
-    const { getByPlaceholderText } = render(
-      <Header name={name} month={12} year={2012} onNameChange={noop} onSignatureChange={noop} />
-    )
+    const { getByPlaceholderText } = renderHeader(2012, 12)
     const field = getByPlaceholderText(/your name/i) as HTMLInputElement
     expect(field.value).toBe(name)
   })
@@ -42,7 +36,7 @@ describe('Header', () => {
   it('handles name field changes', () => {
     const fn = jest.fn()
     const { getByPlaceholderText } = render(
-      <Header month={12} year={2012} onNameChange={fn} onSignatureChange={noop} />
+      <Header month={12} year={2012} onNameChange={fn} onSignatureChange={jest.fn()} />
     )
     const field = getByPlaceholderText(/your name/i) as HTMLInputElement
     const name = 'Test Name'
