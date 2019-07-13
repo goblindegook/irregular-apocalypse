383023d3020d5db95c48aeb2bac9df35b702e551
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-07-13 14:00:38 +0100

    Netlify plugin

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,6 @@
 export default {
+  plugins: ['preact-cli-plugin-netlify'],
+
   webpack(config, env, helpers, options) {
     helpers.getLoadersByName(config, 'postcss-loader').forEach(({ loader }) => {
       delete loader.options
